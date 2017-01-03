a5803c25e6b834bf983466a1bf781555111e4e12
David Lee <dj6lee@uwaterloo.ca>
2017-01-02 21:24:48 -0500

    Updating Angular static page to use nginclude

--- Angular/js/index.js
+++ Angular/js/index.js
@@ -1,5 +1,47 @@
 
 var app = angular.module('app', []);
 app.controller('indexController', function($scope) {
-    $scope.message = 'Hello World';
+    $scope.jobs = [
+        {
+            title: 'Toronto Transit Commision',
+            subtitle: 'C#.NET, Oracle & Crystal Reports Developer',
+            imgUrl: '../Resources/assets/TTCLOGO.png'
+        },
+        {
+            title: 'IBI Group',
+            subtitle: 'Junior Developer',
+            imgUrl: '../Resources/assets/IBIGROUPLOGO.jpg'
+        },
+        {
+            title: 'Veeva Systems',
+            subtitle: 'Intern Performance Engineer',
+            imgUrl: '../Resources/assets/VEEVALOGO.jpg'
+        }
+    ];
+    $scope.projects = [
+        {
+            title: 'Pebble Watchface',
+            link: 'https://apps.getpebble.com/applications/55aa77fbc380d0dd7c00009e',
+            imgUrl: '../Resources/assets/PebblePulseSS.png',
+            wip: false
+        },
+        {
+            title: 'List Application',
+            link: 'https://github.com/davidjameslee/ListApp',
+            imgUrl: '../Resources/assets/ListAppSS.JPG',
+            wip: true
+        },
+        {
+            title: 'Trinket Keyboard',
+            link: 'keyboard.html',
+            imgUrl: '../Resources/assets/KeyboardImg.jpg',
+            wip: false
+        },
+        {
+            title: 'Personal Website',
+            link: '',
+            imgUrl: '../Resources/assets/clouds.jpg',
+            wip: false
+        }
+    ]
 });
