2acf8f6eb75440bff432d90c25448b300f825fde
David Lee <dj6lee@uwaterloo.ca>
2017-01-05 20:32:02 -0500

    implement vue js album gallery

--- Vue/js/album-gallery.js
+++ Vue/js/album-gallery.js
@@ -1,62 +1,49 @@
-$(function() {
+var albumComponent = {
+    props: ['href', 'src'],
+    template: '<a :href="href" target="_blank"><img class="album-art" :src="src || \'../Resources/assets/missingalbum.png\'" /></a>'
+};
 
-    //Get sizes and setup the pictures
-    var setupGallery = function() {
+var app = new Vue({
+    el: '#main',
+    data: {
+        albums: []
+    },
+    components: {
+        'album': albumComponent
+    },
+    mounted: function() {
         var desc = document.getElementById("description");
         var height = window.innerHeight - desc.clientHeight;
         var vCount = Math.max(3, Math.floor(height/160));
         var hCount = Math.floor(window.innerWidth/160);
         var albumCount = vCount * hCount;
-
-        var albums = new Array(albumCount);
-
-        //Get last.fm data for album arts
-        $.ajax({
-            type: 'POST',
-            url: 'http://ws.audioscrobbler.com/2.0/',
-            data: 'method=user.getRecentTracks&' +
-            'limit=' + albumCount + '&' +
-            'user=TheBroccOLee&' +
-            'api_key=4bf436262dec3163d512d025bfd1363d&' +
-            'format=json',
-            dataType: 'jsonp',
-            success: function(data){
-                var album;
-                var link;
-                //document.getElementById("music-info").innerHTML = JSON.stringify(data,undefined, 2);
-
-                for(var i = 0; i < albumCount; i++) {
-                    //Create the album art image
-                    album = document.createElement('img');
-                    link = document.createElement('a');
-                    listEntry = document.createElement('li');
-
-                    link.href = data.recenttracks.track[i].url;
-                    link.target = '_blank';
-
-                    album.className = 'album-art';
-
-                    if(data.recenttracks.track[i].image[2]["#text"].length > 0) {
-                        album.src = data.recenttracks.track[i].image[2]["#text"];
-                    } else {
-                        album.src = '../Resources/assets/missingalbum.png';
-                    }
-
-                    //Add album to link and link to array
-                    link.appendChild(album);
-                    albums[i] = link;
+        this.setupGallery(albumCount)
+    },
+    methods: {
+        setupGallery: function(albumCount) {
+            var that = this;
+            $.ajax({
+                type: 'POST',
+                url: 'http://ws.audioscrobbler.com/2.0/',
+                data: 'method=user.getRecentTracks&' +
+                'limit=' + albumCount + '&' +
+                'user=TheBroccOLee&' +
+                'api_key=4bf436262dec3163d512d025bfd1363d&' +
+                'format=json',
+                dataType: 'jsonp',
+                success: function(data){
+                    that.albums = data.recenttracks.track.map(function(album) {
+                        return {
+                            link: album.url,
+                            imgUrl: album.image[2]['#text']
+                        }
+                    });
+                },
+                error: function(code, message) {
+                    //RIP LASTFM API
+                    alert('Error Retrieving music');
                 }
-                for(var i = 0; i < albumCount; i++){
-                    main.appendChild(albums[i]);
-                }
-            },
-            error: function(code, message) {
-                //RIP LASTFM API
-                alert('Error Retrieving music');
-            }
-        })
-    };
-
-    setupGallery();
-
-})
+            })
+        }
+    }
+});
